81921c076dbc47a38fe2b8bde275201b74c7aefb
ulmetum <ulmetum@gmail.com>
2024-11-15 01:16:35 +0100

    Animacion en el mail de la pagina de Contacto

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,9 @@ import tailwind from '@astrojs/tailwind'
 
 import netlify from '@astrojs/netlify'
 
-import node from '@astrojs/node'
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'hybrid',
-  adapter: netlify()
+	integrations: [tailwind()],
+	output: 'hybrid',
+	adapter: netlify()
 })
